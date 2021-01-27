654bd6bc493cd2db8c932d08ee868469c069835d
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2021-01-27 18:26:12 +0300

    Every assignment is done and completed

--- BMI/Asset/Js/app.js
+++ BMI/Asset/Js/app.js
@@ -20,18 +20,18 @@ let personProfile = {
 };
 
 // Receive the values from input and assign to object properties
-personProfile.firstName = prompt("Enter Your First Name");
-personProfile.lastName = prompt("Enter Your Last Name");
-personProfile.job = prompt("What is Your Profession ?");
-personProfile.birthYear = prompt("Enter Your Birth Date");
+// personProfile.firstName = prompt("Enter Your First Name");
+// personProfile.lastName = prompt("Enter Your Last Name");
+// personProfile.job = prompt("What is Your Profession ?");
+// personProfile.birthYear = prompt("Enter Your Birth Date");
 personProfile.weight = prompt("Your Weight in Kg  ? ");
 personProfile.height = prompt("Your Height in M  ? ");
-let numberOfFamily = prompt("Number of Family  ? ");
+// let numberOfFamily = prompt("Number of Family  ? ");
 
-//Receiving the family number 
-for (let i = 0; i < parseInt(numberOfFamily); i++) {
-    personProfile.familyMembers[i] = prompt("Your Family  Members " + (i + 1));
-}
+// //Receiving the family number 
+// for (let i = 0; i < parseInt(numberOfFamily); i++) {
+//     personProfile.familyMembers[i] = prompt("Your Family  Members " + (i + 1));
+// }
 
 // call age and check vote fun
 personProfile.ageCalc();
@@ -39,20 +39,20 @@ personProfile.checkVote();
 // Adding Self Invoking Function Expression 
 (function () {
 
-    console.log("**************************************************************")
-    console.log("Here is your Profile ")
-    console.log("Full Name: " + personProfile.firstName + " " + personProfile.lastName);
-    console.log("Profession : " + personProfile.job);
-    console.log("Age : " + personProfile.age + " " + "years old");
-    console.log("Is Eligible to Vote : " + personProfile.isEligibleToVote);
-    console.log("Family Members ");
-    //Displaying the family member with foreach
-    personProfile.familyMembers.forEach(function (member, index) {
-        console.log("Family Member  " + (index + 1) + " : " + member);
-    });
+    // console.log("**************************************************************")
+    // console.log("Here is your Profile ")
+    // console.log("Full Name: " + personProfile.firstName + " " + personProfile.lastName);
+    // console.log("Profession : " + personProfile.job);
+    // console.log("Age : " + personProfile.age + " " + "years old");
+    // console.log("Is Eligible to Vote : " + personProfile.isEligibleToVote);
+    // console.log("Family Members ");
+    // //Displaying the family member with foreach
+    // personProfile.familyMembers.forEach(function (member, index) {
+    //     console.log("Family Member  " + (index + 1) + " : " + member);
+    // });
     // call bmi calculator 
     console.log("Your BMI : " + personProfile.calcBmi(personProfile.weight,personProfile.height)); 
-    console.log("**************************************************************")
+    // console.log("**************************************************************")
 
 })();
 
--- Bank/Asset/Js/app.js
+++ Bank/Asset/Js/app.js
@@ -1,66 +1,47 @@
-let userAmount;
-
-function deposit(amt){
-    return userAmount + parseFloat(amt); 
-}
-
-function withdraw(amt){
-    if (userAmount - amt < 0 ){
-            console.log("User can not withdraw this amount. your balance is "+ userAmount);
-    }
-    else if(amt>10000){
-        console.log("User can not withdraw this amount of money.")
-    }
-    else{
-        return userAmount - amt;
-    }
-}
-
-function balance(){
-    return userAmount;
-}
-
-function transfer (amt){
-    if (userAmount - amt < 0 ){
-        console.log("User can not withdraw this amount. your balance is "+ userAmount);
-    }
-    else{
-        return userAmount - amt;
-    }
-}
-
 (function(){
     userAmount = 1000;
     let ask = prompt("Welcome, What do you want to perform. \r\n 1. Balance \r\n 2. Withdraw \r\n 3. Deposit \r\n 4. Transfer");
     if(ask == 1){
         console.log("Your account balance is: "+balance());    
     }
-    if (ask == 2){
-        let asked = prompt("How much money do you want to withdraw?");
-        withdraw(asked);
-        console.log("YOu have successfully withdrawn " + asked + " amount of money.")
-        console.log("Your current account balance is: "+balance());    
-        
+    else if(ask == 2){
+        let amt = prompt("How much money do you want to withdraw? \r\nMaximum: 10,000 ");
+        parseFloat(amt);
+        if (userAmount - amt < 0 ){
+            console.log("User can not withdraw this amount. your balance is "+ userAmount);
+        }
+        else if(amt>10000){
+            console.log("User can not withdraw this amount of money.")
+        }
+        else{
+            let newUserAmount = userAmount - amt;
+            console.log("You have successfully withdrawn " + amt + " birr.") ;
+            console.log("Current balance " + newUserAmount)
+        }
     }
-    if (ask == 3){
-        let asked = prompt("How much money do you want to Deposit?");
-        deposit(asked);
+    else if (ask == 3){
+        let amt = prompt("How much money do you want to Deposit? ");
+        let newUserAmount = userAmount + parseFloat(amt); 
+        console.log("You have successfully deposited "+ amt + " birr");
+        console.log("Your current account balance: " + newUserAmount);
     }
-    if (ask == 4){
-        let asked = prompt("How much money do you want to Transfer?");
-        transfer(asked);
+    else if (ask == 4){
+        let amt = prompt("How much money do you want to Transfer?");
+        let acc = prompt("Enter the account number: \r\ni.e. account number is 10 digits");
+        if (acc.length != 10){
+            console.log("Bank account number is invalid");
+        }
+        else if (userAmount - amt < 0 ){
+            console.log("User can not withdraw this amount. your balance is "+ userAmount);
+        }
+        else{
+            let newBalance = userAmount - parseFloat(amt);
+            console.log("You have transferred amount " + amt+ " to " + parseInt(acc));
+            console.log("Your current balance: "+ newBalance);
+        }
     }
-if(ask == 5){
-    let amt = prompt("How much money do you want to withdraw?");
-    if (userAmount - amt < 0 ){
-        console.log("User can not withdraw this amount. your balance is "+ userAmount);
-}
-else if(amt>10000){
-    console.log("User can not withdraw this amount of money.")
-}
-else{
-    console.log(userAmount - amt) ;
-}
-}
+    else{
+        console.log("Incorrect input.")
+    }
 
 })();
